5b625491efe24f22fc8350457ae9699b0fcbd467
David Jablonski <djablonski@trave.mip.informatik.uni-kiel.de>
2014-11-04 16:39:57 +0100

    Grab an image implemented.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <Base/Common/W32Compat.hh> // sprintf wrn
+#include <VideoSource/VideoSource_OpenNI.hh>
+#include <VideoSource/VideoSourceCapabilities.hh>
+#include <Base/Image/Image.hh>
+#include <Base/Image/ImageIO.hh>
+#include <Image/Camera.hh>
+#include <vector>
+#include <string>
 
-int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+
+int main(int argc, char**argv) {
+  BIAS::VideoSource_OpenNI* cam = new BIAS::VideoSource_OpenNI;
+  cam->SetDepthCaptureMode(BIAS::VideoSource_Kinect_Base::DepthEuclidean);
+  BIAS::Camera<float> img;
+  cam->StartDepthStream();
+  cam->OpenDevice();
+  cam->CloseDevice();
+  cam->InitImage(img);
+  cam->PreGrab();
+  cam->GrabSingleDepth(img);
+  cam->PostGrab();
+  cam->StopDepthStream();
+  cam->CloseDevice();
+  delete(cam);
+  BIAS::ImageIO::ExportLibJPEG("test.jpg",img);
+  return 0;
 }
