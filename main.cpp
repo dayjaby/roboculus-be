bbeb50a76022e1dc18ff0c09dd98b03092fd3e1b
David Jablonski <dayjaby@gmail.com>
2014-11-18 12:59:40 +0100

    fixed openni depth camera class

--- main.cpp
+++ main.cpp
@@ -11,45 +11,61 @@
 #include <string>
 
 namespace BIAS {
+
 class VideoSource_OpenNI_Depth : public BIAS::VideoSource_OpenNI
 {
 public:
-  VideoSource_OpenNI_Depth() 
+  VideoSource_OpenNI_Depth()
   {
     SetDepthCaptureMode(BIAS::VideoSource_Kinect_Base::DepthEuclidean);
-    std::cout << "initf" << std::endl;
-    tmp.Init(640,480,1,ImageBase::ST_float);
-    std::cout << "initf" << std::endl;
+    tmp.Init(width,height,depth_channels,ImageBase::ST_float);
     tmp.SetColorModel(ImageBase::CM_Depth);
-    std::cout << "initf" << std::endl;
-    //InitDepthImage(tmp);
+    InitDepthImage(tmp);
   }
   virtual ~VideoSource_OpenNI_Depth() {
   }
-  
+
   virtual int GrabSingle(BIAS::Camera<unsigned char>& image) {
-    std::cout << "Grab single" << std::endl;
     int result = BIAS::VideoSource_OpenNI::GrabSingleDepth(tmp);
-    std::cout << "Grab single" << std::endl;
-    BIAS::ImageConvert::ConvertST(tmp,image,BIAS::ImageBase::ST_unsignedchar);
-    std::cout << "Grab single" << std::endl;
+    /*ImageConvert::ConvertST is inconvenient for our purpose,
+      because it merely casts the float to unsigned char,
+      which is a mistake for values>255
+      BIAS::ImageConvert::ConvertST(tmp,image,BIAS::ImageBase::ST_unsignedchar);*/
+    image.Init(width, height, convert_channels, BIAS::ImageBase::ST_unsignedchar);
+    /* do some dirty float to 2* unsigned char converting
+       the value of the depth_channel is the distance in millimeters,
+       thus it's not a problem to omit the decimal place for our purpose
+       bytes to send per second: 30*640*480*2 ~= 18MB!
+    */
+    /// TODO: consider using only 1 convert channel to use only half the bandwidth
+    for(int x=0;x<width;x++) {
+        for(int y=0;y<height;y++) {
+            int v = tmp.GetValue<int>(tmp,x,y,0);
+            if(v>65535) v = 65535;
+            // two convert channels
+            image.SetValue(image,x,y,0,(unsigned char)v&0xFF);
+            image.SetValue(image,x,y,1,(unsigned char)(v>>8)&0xFF);
+        }
+    }
     return result;
   }
 private:
+  // constant values, don't change!
+  enum { width = 640, height = 480, depth_channels = 1, convert_channels = 2};
   BIAS::Camera<float> tmp;
 };
 
 class FPSVideoServer : public BIAS::VideoServer
 {
 public:
-  FPSVideoServer(int fps) 
+  FPSVideoServer(int fps)
   : fps_(fps)
   , usecPerFrame_(1000000.0/fps)
   {
   }
-  
-  virtual ~FPSVideoServer() {} 
-  
+
+  virtual ~FPSVideoServer() {}
+
   int Process() {
     timer_.Start();
     int res=ProcessOneImage();
@@ -63,48 +79,12 @@ public:
       biasusleep(waitTime);
       realTime+=waitTime;
     }
-    
+
     realFps_ = 1000000.0f / float( realTime );
     timer_.Reset();
     return res;
-  nitFromExistingSource(VideoSource *theSource, int port) {
-133  int res;
-134  if (port!=port_)
-135  port_=port;
-136  cout <<"Initializing VideoServer on port:"<<port_<<endl;
-137  camera_ = theSource;
-138  if (!camera_->IsActive()) {
-139  res = camera_->OpenDevice();
-140  if (res!=0) return res;
-141  res = camera_->PreGrab();
-142  if (res!=0) return res;
-143  }
-144 
-145  imgWidth_=camera_->GetWidth();
-146  imgHeight_=camera_->GetHeight();
-147 
-148 #ifdef BIAS_HAVE_LIBJPEG
-149  if (jpeg_ > 0) {
-150  if (Compressor_.Init() < 0) {
-151  BIASERR("Could not initialize JPEG-compression!!!");
-152  return -1;
-153  }
-154  }
-155 #else //BIAS_HAVE_LIBJPEG
-156  if (jpeg_ > 0) {
-157  BIASERR("Compression mode not available without LIBJPEG!");
-158  }
-159 #endif //BIAS_HAVE_LIBJPEG
-160  if (useUDP_){
-161  // Transmitter_.Connect(TargetAddress_->c_str(),*SendPort_);
-162  } else {
-163  server_.WaitForConnections(port_);
-164  }
-165  camera_->InitImage(CamImage_);
-166  cout<<"Grabbing image size: "<< imgWidth_<<"x"<< imgHeight_<<endl;
-167  Initialized_ = true;
-168  return 0;
-169 }}
+  }
+
   double getFPS() { return fps_; }
   double getRealFPS() { return realFps_; }
   double getUsecPerFrame() { return usecPerFrame_; }
@@ -126,7 +106,7 @@ int main(int argc, char**argv) {
   // init a video server with 30 FPS, because the kinect camera provides 30 FPS
   BIAS::FPSVideoServer server(30);
   std::cout << server.InitFromExistingSource(&cam,44887) << std::endl;
-  
+
   while(true) {
     if(server.Process() == 0) {
       std::cout << "Image sent with " << server.getRealFPS() << " FPS" << std::endl;
@@ -134,5 +114,7 @@ int main(int argc, char**argv) {
       //std::cout << "No client connected; " << server.getRealFPS() << " FPS" << std::endl;
     }
   }
+  cam.CloseDevice();
+
   return 0;
 }
